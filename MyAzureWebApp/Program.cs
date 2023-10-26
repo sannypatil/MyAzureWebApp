17d9f14f8d0ce87f672e7dea6103d03868250a02
SPatil64 <santhosh.patil64@t-mobile.com>
2023-10-26 11:00:29 +0530

    Updated Swagger

--- MyAzureWebApp/Program.cs
+++ MyAzureWebApp/Program.cs
@@ -9,10 +9,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Google API", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
 });
 
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
